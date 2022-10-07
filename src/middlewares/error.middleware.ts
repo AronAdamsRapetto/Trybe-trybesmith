53bc8aaa13a6395adf18c7e1e0235dc276a3011d
Aron Adams <aronrapetto@hotmail.com>
2022-10-07 02:45:48 -0300

    feat: error middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+import { HttpException } from '../utils/errorThrower';
+
+const errorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.log(err);
+  const { statusCode, message } = err as HttpException;
+  if (statusCode) res.status(statusCode).json({ message });
+  res.status(500).json({ message: 'Erro interno' });
+};
+
+export default errorMiddleware;
